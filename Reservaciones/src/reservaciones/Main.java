b6403b72677ff78c4179396407ebd5e9d010434d
MrPiipe <piipe.donado@hotmail.com>
2015-05-27 16:25:42 -0500

    Paquetes

--- Reservaciones/src/reservaciones/Main.java
+++ Reservaciones/src/reservaciones/Main.java
@@ -8,7 +8,7 @@ package reservaciones;
 public class Main {
 
     public static void main(String[] args){
-        Frame frame = new Frame();
+        //Frame frame = new Frame();
         Sql sql = new Sql();
     }
 }
--- Reservaciones/src/reservaciones/Sql.java
+++ Reservaciones/src/reservaciones/Sql.java
@@ -4,13 +4,26 @@
  * and open the template in the editor.
  */
 package reservaciones;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.*; 
 
 public class Sql {
-    
     Sql(){
-
+        try {
+            String host = "jdbc:mysql://sql3.freesqldatabase.com:3306/sql378796";
+            String uName = "sql378796";
+            String uPass = "gG6!jZ8*";
+            Connection con = DriverManager.getConnection(host, uName, uPass);
+            Statement stmt = con.createStatement( );
+            String SQL = "SELECT * FROM Restaurantes";
+            ResultSet rs = stmt.executeQuery( SQL );
+            
+        }catch( SQLException err ) {
+            System.out.println( err.getMessage( ) );        
+        }
     }
-
 }
