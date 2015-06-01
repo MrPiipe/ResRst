626f3356cb0a5d959fc808210799864efa70be5f
alejandro <asalgad2@eafit.edu.co>
2015-06-01 12:15:32 -0500

    cambiando comando

--- script.sh
+++ script.sh
@@ -6,9 +6,7 @@ run2(){
     program=$1
     cd ..
     javac reservaciones/$1
-    cd reservaciones
-    mv *.class ../../build/classes/reservaciones
-    cd ../../build/classes   
     java reservaciones.${program/.java/""}
-    bd
+    cd reservaciones
+    rm reservaciones/*.class    
 }
