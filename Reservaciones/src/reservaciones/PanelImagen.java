cc748e7db2335ce736252cb9d46a5b952d1b5a71
johan yepes <johanyepes@hotmail.com>
2015-06-05 23:42:02 -0500

    Mostrando Imagenes

---
+++ Reservaciones/src/reservaciones/PanelImagen.java
@@ -0,0 +1,29 @@
+package reservaciones;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class PanelImagen extends JPanel{
+
+    String nombreImagen;
+    
+    protected PanelImagen(String nombreImagen) {
+        this.nombreImagen = nombreImagen;
+        setLayout(null);
+    }
+    
+    
+    @Override
+    public void paint(Graphics grafico){
+        repaint();
+        Dimension tamaño = getSize();
+        System.out.println(nombreImagen);
+        ImageIcon img = new ImageIcon(getClass().getResource("/Imagenes/"+nombreImagen+".png"));
+        grafico.drawImage(img.getImage(), 0, 0, tamaño.width, tamaño.height, null);
+        setOpaque(false);
+        super.paintComponent(grafico);
+    }
+    
+}
